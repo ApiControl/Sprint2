e4c03e91c99d62e80ccbb9fef5f364788e232d12
Gabrielle Śimões <gabrielle.goncalves@sptech.school>
2023-09-25 19:39:23 -0300

    Modificações do Banco de dados realizadas com base na Modelagem. Selects não foram alterados pois serão discutidos posteriormente

--- ApiControl_Old/Banco de dados APICONTROL/Tabelas Apicontrol ofc.sql
+++ ApiControl_Old/Banco de dados APICONTROL/Tabelas Apicontrol ofc.sql
@@ -2,45 +2,46 @@ create database apicontrol;
 
 use apicontrol;
 
--- TABELA CADASTRO
+-- Tabela USUÁRIO
 create table usuario(
 id_usuario int primary key auto_increment,
 nome varchar(50),
 email varchar(100) unique,
-senha varchar(50),
-cnpj char (14),
+senha varchar(45),
+cnpj char(14),
 data_cadastro datetime default current_timestamp,
-telefone varchar (9)) auto_increment = 1000;
-
--- Adicionado a coluna fk_endereco para receber a chave estrangeira da tabela endereco
-alter table usuario add column fk_endereco int;
-
--- Determinando a coluna fk_endereco como a cheve estrangeria da minha tabela usuario
-alter table usuario add constraint fkEnd 
-	foreign key (fk_endereco)
-		references endereco  (id_endereco);
+telefone char(12),
+fk_endereco int  -- Adicionado a coluna fk_endereco para receber a chave estrangeira da tabela endereco
+);
 
 desc usuario;
 
-insert into usuario(nome, email, senha, cnpj, telefone, fk_endereco) values 
-('Apiario Caminho do Sol', 'caminhodosol@gmail.com', '123456789', '12345678965412', '71956478922', 50),
-('Familia do mel', 'caminho@gmail.com', '123456788', '12345678965XXX', '71952418677', 51),
-('Mel e companhia', 'companhia@gmail.com', '1234569878','56412698745215', '11956325475', 52);
+-- Dados mocados na tabela USUARIO
+insert into usuario (nome, email, senha, cnpj, telefone) values 
+('Apiario Caminho do Sol', 'caminhodosol@gmail.com', '123456789', '12345678965412', '71956478922'),
+('Familia do mel', 'caminho@gmail.com', '123456788', '12345678965XXX', '71952418677'),
+('Mel e companhia', 'companhia@gmail.com', '1234569878','56412698745215', '11956325475');
 
 select * from usuario;
 
+-- Tabela ENDEREÇO
 create table endereco (
 id_endereco int primary key auto_increment,
 estado char(2),
-cidade varchar (40),
+cidade varchar(40),
 cep char(8),
-bairro varchar (40),
-logradouro varchar (100),
-numero varchar (10),
-complemento varchar (20)
-) auto_increment = 50;
+bairro varchar(40),
+logradouro varchar(100),
+numero varchar(15),
+complemento varchar(20)
+) auto_increment = 100;
  
+-- Determinando a coluna fk_endereco como a cheve estrangeria da minha tabela usuario
+alter table usuario add constraint fkEnd 
+	foreign key (fk_endereco) 
+		references endereco (id_endereco);
 
+-- Dados mocados na tabela endereço
 insert into endereco (estado, cidade, cep, bairro, logradouro, numero, complemento) values
 	('BA', 'Salvador', '40015110', 'Rua Julio Carreira', 'Comércio', '55', null),
     ('BA', 'Salvador', '40015035', 'Comércio',' Avenida Jequitaia', 45, null ),
@@ -48,104 +49,46 @@ insert into endereco (estado, cidade, cep, bairro, logradouro, numero, complemen
   
 select * from endereco;
 
+update usuario set fk_endereco = 100
+	where id_usuario = 2;
+    
+update usuario set fk_endereco = 101
+	where id_usuario = 1;
+    
+update usuario set fk_endereco = 102
+	where id_usuario = 3;
+    
+select * from usuario;
+
+-- Tabela SENSOR (terá dados adicionados posteriormente pelo aurduino)
 create table sensor(
 id_sensor int primary key auto_increment,
 temperatura float,
-data_instalacao date,
 data_hora_coleta datetime default current_timestamp
-) auto_increment = 100;
-
-insert into sensor (data_instalacao) values
-	('2023-09-14'),
-	('2023-09-14'),
-    ('2023-09-14');
+);
     
 desc sensor;
 
+insert into sensor (temperatura) values
+(32),
+(22),
+(27),
+(18);
+
 select * from sensor;
 
--- TABELA COLMEIA
-create table colmeia(
-id_colmeia int primary key auto_increment,
-especie varchar (50),
-descricao varchar(200));
-
--- Adicionando uma coluna localizacao na tabela colmeia
-alter table colmeia add column localizacao varchar(20);
-
--- Adicionar valores a coluna localização da tabela colmeia
-update colmeia set localizacao = 'corredor 1'
-	where id_colmeia = 1;
-update colmeia set localizacao = 'corredor 2'
-	where id_colmeia = 2;
-update colmeia set localizacao = 'corredor 3'
-	where id_colmeia = 3;
-update sensor set temperatura = 38.5
-	where id_sensor = 103;
-    
-select * from colmeia;
-
-update colmeia set fk_especie = 200
-	where id_colmeia in(1,2,3,4);
-update colmeia set fk_especie = 203
-	where id_colmeia in (5,6);
-
--- adicionando uma constrain na colmeia
-Alter table colmeia add column status_colmeia varchar(15)
-	constraint chkstatus check (status_colmeia in ('Ideal','Médio','Crítico'));
-
--- Adicionando a coluna para receber a chave estrangeira da tabela colmeia
-Alter table colmeia add column fksensor_id int;
-
--- Definido a coluna fksensor para ser a chave estrangeira, com referencia no idSensor da tabela sensor
-alter table colmeia add constraint fk_sensor
-	foreign key (fksensor_id)
-		references  sensor(id_sensor);
-        
--- Adicionado outra coluna para chave estrageira onde vai ligar o usuario ao enxame, detectando que ele é o dono daquele enxame
-alter table colmeia add column fk_usuario int;
-
--- definindo a coluna que criei para ser a chave estrangerio para a chave de fato buscando a referencia do ID da tabela usuario
-alter table colmeia add constraint fkUsuario
-	foreign key (fk_usuario)
-		references usuario(id_usuario);
-
-desc colmeia;
-
-insert into colmeia (especie, descricao, status_colmeia, fksensor_id) values
-	('Jataí', 'Colmeia Revisada recentemente','Ideal', 103),
-    ('Mandaçaia', 'Colmeia Revisada no ano de 2023 ', 'Crítico', 104),
-    ('Uruçu', 'esta colmeia produziu bem ano passado', 'Médio', 105);
-    
-insert into colmeia (especie, descricao, status_colmeia, fksensor_id, fk_usuario) values
-	('Apis', 'Enxame sem Rainha', 'Critico', 106, 1002),
-    ('Apis', 'Enxame forte', 'Ideal', 107, 1002),
-    ('Apis', 'Enxame fraco', 'Crítico', 108, 1002);
-
--- asociando o a colmeia ao id do usuario
-update colmeia set fk_usuario = 1000
-	where id_colmeia = 1;
-update colmeia set fk_usuario = 1000
-	where id_colmeia = 2;
-update colmeia set fk_usuario = 1001
-	where id_colmeia = 3;
-
-select * from colmeia;
-    
-    
-    
+-- Tabela ESPECIE
 create table especie (
 id_especie int primary key auto_increment,
 nome varchar (40),
-tipo varchar(30),
+tipo varchar(40),
 media_producao_ano float null,
-temperatura_ideal float null,
-umidade_ideal float null,
-media_valor_mel_kg float null)auto_increment = 200;
-
-drop table especie;
-
-
+temperatura_minima float null, -- default null
+temperatura_maxima float null,
+umidade_minima float null,
+umidade_maxima float null,
+media_valor_mel_kg float null
+)auto_increment = 100;
 
 insert into especie (nome, tipo, media_producao_ano, media_valor_mel_kg) values
 	('Abelha Europeia', 'Apis mellifera', 28, 30),
@@ -156,22 +99,52 @@ insert into especie (nome, tipo, media_producao_ano, media_valor_mel_kg) values
 
 select * from especie;
 
--- Apagando a coluna especie da tabela colmeia para criar outra que vem de fora
-alter table colmeia drop column especie;
+-- Tabela COLMEIA
+create table colmeia(
+id_colmeia smallint primary key auto_increment,
+descricao varchar(200),
+status_colmeia varchar(40),
+localizacao varchar(40),
+fk_sensor int,
+fk_usuario int,
+fk_especie int
+) auto_increment = 1000; 
 
--- adicionando a coluna para receber a chave estrageira 
-alter table colmeia add column fk_especie int;
+-- Definido a coluna fksensor para ser a chave estrangeira, com referencia no idSensor da tabela sensor
+alter table colmeia add constraint fkssr
+	foreign key (fk_sensor)
+		references  sensor(id_sensor);
 
--- auterando a coluna para ser uma chave estrajeira
-alter table colmeia add constraint fkesp
+-- Definindo a coluna fkusuario para ser a chave estrangeria com a referencia do ID da tabela usuario
+alter table colmeia add constraint fkusr
+	foreign key (fk_usuario)
+		references usuario (id_usuario);
+        
+-- Definindo a coluna fkusuario para ser a chave estrangeria com a referencia do ID da tabela usuario
+alter table colmeia add constraint fkepc
 	foreign key (fk_especie)
-		references especie(id_especie);
+		references especie (id_especie);
+        
+-- adicionando uma constrain na colmeia
+Alter table colmeia add constraint chkstatus 
+	check (status_colmeia in ('Ideal','Médio','Crítico')
+);
 
+desc colmeia;
 
+insert into colmeia (descricao, status_colmeia, localizacao, fk_sensor, fk_usuario, fk_especie) values
+	('Colmeia Revisada recentemente','Ideal', 'Corredor 3', 1, 1, 103),
+    ('Colmeia Revisada no ano de 2023 ', 'Crítico', 'Coredor 1', 4, 2, 101),
+    ('Esta colmeia produziu bem ano passado', 'Médio', 'Corredor 2', 2, 3, 102);
 
+select * from colmeia;
 
 -- TABELAS API CONTROL
 show tables;
+
+
+
+
 
 -- Exibindo a especie da abela a localizaçãoe e o sensor associado a essa colmeia
 select colmeia.especie as ESPECIE_ABELHA, colmeia.localizacao as CORREDOR_COLMEIA, sensor.id_sensor as ID_SENSOR
@@ -205,7 +178,6 @@ select * from usuario;
 select * from sensor;
 select * from especie;
 
-
 truncate table colmeia;
 
-desc colmeia
+desc colmeia;
